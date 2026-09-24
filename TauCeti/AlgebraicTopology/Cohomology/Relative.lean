d544a24ef084863aff8e331bc691d9d72f883469
√2 <240895760+sqrt-of-2@users.noreply.github.com>
2026-09-24 00:47:13 +0000

    fix: remove redundant cohomology simp rules

--- TauCeti/AlgebraicTopology/Cohomology/Relative.lean
+++ TauCeti/AlgebraicTopology/Cohomology/Relative.lean
@@ -166,7 +166,7 @@ abbrev singularCohomologyδ (n m : ℕ) (h : n + 1 = m := by lia) :
     P.snd.singularCohomology R k M n ⟶ P.singularCohomology R k M m :=
   (P.shortExact_singularCochainComplexShortComplex R k M).δ n m (by simpa)
 
-@[reassoc (attr := simp)]
+@[reassoc]
 lemma singularCohomologyδ_comp_singularCohomologyπ (n m : ℕ) (h : n + 1 = m := by lia) :
     P.singularCohomologyδ R k M n m h ≫ P.singularCohomologyπ R k M m = 0 :=
   (P.shortExact_singularCochainComplexShortComplex R k M).δ_comp n m (by simpa)
