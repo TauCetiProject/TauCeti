c6ace2626b3789404e629255a4750ed8cd214efa
√2 <240895760+sqrt-of-2@users.noreply.github.com>
2026-09-25 04:07:19 +0000

    fix: simplify relative cohomology sequence equations

--- TauCeti/AlgebraicTopology/Cohomology/Relative.lean
+++ TauCeti/AlgebraicTopology/Cohomology/Relative.lean
@@ -173,6 +173,7 @@ lemma singularCochainComplexShortComplex_eq_map :
 
 /-- The first map of the cochain sequence is the image under `Hom(-, M)` of the quotient map
 from ambient to relative singular chains. -/
+@[simp]
 lemma singularCochainComplexShortComplex_f_eq :
     (P.singularCochainComplexShortComplex R k M).f =
       (TauCeti.ChainComplex.linearYonedaFunctor k M).map (P.singularChainComplexπ R).op := rfl
@@ -205,7 +206,7 @@ abbrev singularCohomologyδ (n m : ℕ) (h : n + 1 = m := by lia) :
     P.snd.singularCohomology R k M n ⟶ P.singularCohomology R k M m :=
   (P.shortExact_singularCochainComplexShortComplex R k M).δ n m (by simpa)
 
-@[reassoc]
+@[reassoc (attr := simp)]
 lemma singularCohomologyδ_comp_singularCohomologyπ (n m : ℕ) (h : n + 1 = m := by lia) :
     P.singularCohomologyδ R k M n m h ≫ P.singularCohomologyπ R k M m = 0 :=
   (P.shortExact_singularCochainComplexShortComplex R k M).δ_comp n m (by simpa)
